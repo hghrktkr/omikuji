64d2103204a7048aed88889c4e78befce6736c7c
hghrktkr <t.hagihara@anem.co.jp>
2024-06-14 12:53:21 +0900

    2024/06/14 0時にセッション削除

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// 0時にセッションを削除
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::call(function () {
+    DB::table('sessions')->delete();
+})->daily();
