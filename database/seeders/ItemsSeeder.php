b75af5a4634dbcb75a0d69e1d2e284b1511dcad9
hghrktkr <147295919+hghrktkr@users.noreply.github.com>
2025-06-20 15:01:15 +0900

    item seeder

--- database/seeders/ItemsSeeder.php
+++ database/seeders/ItemsSeeder.php
@@ -2,18 +2,34 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Http\Models\Item;
+use App\Models\Item;
+use App\Models\User;
 
 class ItemsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
+        $csvFile = database_path('csv/items.csv'); // CSVファイルパス
+        $rows = array_map('str_getcsv', file($csvFile));
+        $header = array_shift($rows);
 
+        foreach ($rows as $row) {
+            $data = array_combine($header, $row);
+            if (empty($data['email'])) continue;
+
+            // メールからユーザー取得
+            $user = User::where('email', $data['email'])->first();
+
+            if ($user) {
+                Item::create([
+                    'email' => $data['email'],
+                    'rank_name' => $data['rank_name'],
+                    'quantity' => (int)$data['quantity'],
+                    'created_at' => $data['created_at'] ?: now(),
+                    'updated_at' => $data['updated_at'] ?: now(),
+                ]);
+            }
+        }
     }
-    
 }
